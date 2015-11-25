a2c0b59864e8ad0b393815a900219a792b635b60
Jakub Fijałkowski <fiolek94@gmail.com>
2015-11-25 22:58:18 +0100

    Better figure naming in list

--- UI.Utils.fs
+++ UI.Utils.fs
@@ -5,16 +5,48 @@ open System.Windows.Data
 
 open FsRaster.Figures
 
-module UIUtils = 
+module UIUtils =
+    let private toString (a : 'a) = sprintf "%A" a
+    let private getApproxLocation = function
+    | Point (p, _)                -> [ toString p ]
+    | Line (p1, p2, _)            -> [ toString p1; toString p2 ]
+    | Circle (p, r, _)            -> [ toString p; toString r ]
+    | AntialiasedCircle (p, r, _) -> [ toString p; toString r ]
+    | SquareLine (p1, p2, _, _)   -> [ toString p1; toString p2 ]
+    | DiamondLine (p1, p2, _, _)  -> [ toString p1; toString p2 ]
+    | CircleLine (p1, p2, _, _)   -> [ toString p1; toString p2 ]
+    | Polyline (pts, _)           -> [ toString <| List.minBy fst pts ]
+    | Polygon (pts, _)            -> [ toString <| List.minBy fst pts ]
+    | FilledPolygon (pts, _)      -> [ toString <| List.minBy fst pts ]
+    | Brush (pts, _)              -> [ toString <| List.minBy fst pts ]
+    | FilledBrush (pts, _)        -> [ toString <| List.minBy fst pts ]
+    let private getApproxLocationString fig =
+        let loc = getApproxLocation fig
+        match loc with
+        | [ p ] -> p
+        | [ p1; p2 ] -> sprintf "(%s, %s)" p1 p2
+        | _ -> ""
+
+    let private getThickness = function
+    | SquareLine (_, _, _, t)  -> t
+    | DiamondLine (_, _, _, t) -> t
+    | CircleLine (_, _, _, t)  -> t
+    | _                        -> 0
+    let private getThicknessString fig =
+        let th = getThickness fig
+        if th > 0 then sprintf " (Thickness: %d)" th else ""
+
     let private colorName = function
-        | FsRaster.FigureColor.Color c -> sprintf "Color: %A" c
-        | FsRaster.FigureColor.BitmapPattern _ -> "Texture"
+        | FsRaster.FigureColor.Color c -> sprintf "%A" c
+        | FsRaster.FigureColor.BitmapPattern _ -> "Textured"
 
     let private figureName fig = fig.GetType().Name
     let shortDescriptionOf fig = (figureName fig).ToLower()
     let longDescriptionOf fig =
         let desc = getFigureInfo fig
-        sprintf "%s %s" (figureName fig) (colorName desc.Color)
+        let loc = getApproxLocationString fig
+        let thickness = getThicknessString fig
+        sprintf "%s %s %s%s" (figureName fig) loc (colorName desc.Color) thickness
 
 type FigureNameConverter() =
     interface IValueConverter with
