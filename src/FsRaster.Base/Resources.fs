828709c8f24ca7a7e089873fc8fe195038371d11
Jakub Fijałkowski <fiolek94@gmail.com>
2016-01-22 20:04:11 +0100

    Take into account multiple assemblies in resource management

--- src/FsRaster.Base/Resources.fs
+++ src/FsRaster.Base/Resources.fs
@@ -9,23 +9,33 @@ type private SampleType = SampleType
 
 let private cursorCache = new Dictionary<string, Cursor> ()
 
-let getResourceManager =
-    let assembly = System.Reflection.Assembly.GetEntryAssembly()
-    new ResourceManager("Resources", assembly)
+let getResourceManagers =
+    System.AppDomain.CurrentDomain.GetAssemblies()
+    |> Array.filter (fun a -> a.FullName.Contains("FsRaster"))
+    |> Array.map (fun a -> new ResourceManager("Resources", a))
+
+let private getSomething getter name (managers : ResourceManager array) =
+    managers |> Array.map (fun m ->
+        try
+            getter m name
+        with
+            | e -> null
+    )
+    |> Array.where (isNull >> not)
+    |> Array.head
+
+let getObject = getSomething (fun m n -> m.GetObject(n) :?> byte [])
+let getString = getSomething (fun m n -> m.GetString(n))
 
 let loadCursorFile name =
     match cursorCache.TryGetValue name with
     | true, c -> c
     | _ ->
-        let data = getResourceManager.GetObject(name) :?> byte []
+        let data = getObject name getResourceManagers
         use stream = new MemoryStream(data)
         let c = new Cursor(stream)
         cursorCache.[name] <- c
         c
 
-let loadString name =
-    getResourceManager.GetString(name)
-
-let loadStream name =
-    let data = getResourceManager.GetObject(name) :?> byte []
-    new MemoryStream(data)
+let inline loadString name = getString name getResourceManagers
+let inline loadStream name = new MemoryStream(getObject name getResourceManagers)
